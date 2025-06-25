bbe9106f9ceb3cdad41f2599fad0e87b7b1561a0
ArianaGu112 <arianaguchen@gmail.com>
2025-06-26 00:25:39 +0800

    translator.py

---
+++ translator.py
@@ -0,0 +1,18 @@
+from translate import Translator
+import streamlit as st
+
+st.title("Chinese ↔ English Translator")
+text = st.text_area("Enter text:")
+option = st.selectbox("Translate to:", ["chinese", "english"])
+
+if st.button("Translate"):
+  if option=="chinese":
+    translator = Translator(to_lang="zh")
+  elif option=="english":
+    translator = Translator(to_lang="en")
+  try:
+    result = translator.translate(text)
+    st.write("Translation:", result)
+  except Exception as e:
+    st.error(f"Error during translation: {e}")
+
